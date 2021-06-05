d34fd4d932e82374b217d4a88b9c6af296a2f75c
AlanNgaiii <634620668@qq.com>
2021-06-05 16:53:18 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul class="subject-list">
+      <li
+        class="subject-item"
+        v-for="(subject, subjectName) in subjectsMap"
+        :key="subjectName"
+      >
+        {{ subjectName }}
+        <ul class="article-list">
+          <li
+            class="article-item"
+            v-for="article in subject"
+            :key="article.href"
+            @click="toArticle(article.fileName)"
+            v-show="!article.needPay"
+          >
+            {{ article.title }}
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import subjectsMap from "@/config/subjectsMap.json";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      subjectsMap,
+      host: 'https://alanngai1996.xyz/store/w3c-plus-free/htmls/'
+    };
+  },
+  methods:{
+    toArticle(fileName){
+      window.open(this.host + fileName);
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.subject-list {
+  font-size: 18px;
+
+  .subject-item {
+    border: 1px solid #eee;
+    margin-bottom: 100px;
+    padding: 20px;
+  }
+  .article-list {
+    padding-left: 20px;
+    padding-top: 20px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr) ;
+
+    .article-item {
+      font-size: 16px;
+      margin-bottom: 15px;
+      line-height: 30px;
+      cursor: pointer;
+
+      &:hover{
+        color: red;
+      }
+    }
+  }
 }
 </style>
